a74ffa24797c1fdceb937d108e04200f8bbffd90
강진우 <wlsdn1252@naver.com>
2023-01-14 19:56:38 +0900

    각각의 프래그먼트를 뿌려줄 어댑터클래스 생성

---
+++ app/src/main/java/com/example/viewpagerpractice/adapters/MainViewPagerAdapter.kt
@@ -0,0 +1,18 @@
+package com.example.viewpagerpractice.adapters
+
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentPagerAdapter
+
+class MainViewPagerAdapter(fm : FragmentManager) : FragmentPagerAdapter(fm){
+    override fun getItem(position: Int): Fragment {
+
+    }
+
+    override fun getCount(): Int {
+
+    }
+
+
+
+}
