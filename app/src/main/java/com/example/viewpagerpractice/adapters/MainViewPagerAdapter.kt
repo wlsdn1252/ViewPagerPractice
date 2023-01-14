8197c86454751fbb3df4fe3fbc47b0fd5a7495f5
강진우 <wlsdn1252@naver.com>
2023-01-14 20:23:41 +0900

    페이지별 탭레이아웃 제목 설정

--- app/src/main/java/com/example/viewpagerpractice/adapters/MainViewPagerAdapter.kt
+++ app/src/main/java/com/example/viewpagerpractice/adapters/MainViewPagerAdapter.kt
@@ -9,6 +9,18 @@ import com.example.viewpagerpractice.ftagments.NameFragment
 
 class MainViewPagerAdapter(fm : FragmentManager) : FragmentPagerAdapter(fm){
 
+    // 프래그먼트 페이지마다 탭레이아웃의 제목이 뭔가요
+    override fun getPageTitle(position: Int): CharSequence? {
+        if(position == 0){
+            return "이름"
+        }else if(position == 1){
+            return "내 정보"
+        }else{
+            return "인사말"
+        }
+    }
+
+
     // 몇번의 포지션에따라 어떤 프래그먼트가 나와야하는지 물어보는 함수
     override fun getItem(position: Int): Fragment {
         if(position == 0){
