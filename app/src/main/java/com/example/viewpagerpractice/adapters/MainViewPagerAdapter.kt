5895c118d69645ab6a8da88012897a4e8418c6cc
강진우 <wlsdn1252@naver.com>
2023-01-14 19:59:41 +0900

    포지션이 몇번인지에 따라 어떤 프래그먼트를 뿌려줄지 작성

--- app/src/main/java/com/example/viewpagerpractice/adapters/MainViewPagerAdapter.kt
+++ app/src/main/java/com/example/viewpagerpractice/adapters/MainViewPagerAdapter.kt
@@ -3,14 +3,26 @@ package com.example.viewpagerpractice.adapters
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentPagerAdapter
+import com.example.viewpagerpractice.ftagments.GreetingsFragment
+import com.example.viewpagerpractice.ftagments.MyinfoFragment
+import com.example.viewpagerpractice.ftagments.NameFragment
 
 class MainViewPagerAdapter(fm : FragmentManager) : FragmentPagerAdapter(fm){
-    override fun getItem(position: Int): Fragment {
 
+    // 몇번의 포지션에따라 어떤 프래그먼트가 나와야하는지 물어보는 함수
+    override fun getItem(position: Int): Fragment {
+        if(position == 0){
+            return NameFragment()
+        }else if(position == 1){
+            return MyinfoFragment()
+        }else{
+            return GreetingsFragment()
+        }
     }
 
+    // 총 몇개의 프래그먼트인지 카운트하는 함수
     override fun getCount(): Int {
-
+        return 3
     }
 
 
