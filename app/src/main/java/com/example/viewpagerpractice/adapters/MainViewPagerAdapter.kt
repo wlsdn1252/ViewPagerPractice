497d1316cb8d644324da2e5fab866be58ead2ca9
강진우 <wlsdn1252@naver.com>
2023-01-14 20:27:38 +0900

    if 조건문을 코틀린 조건문 when으로 변경

--- app/src/main/java/com/example/viewpagerpractice/adapters/MainViewPagerAdapter.kt
+++ app/src/main/java/com/example/viewpagerpractice/adapters/MainViewPagerAdapter.kt
@@ -11,24 +11,20 @@ class MainViewPagerAdapter(fm : FragmentManager) : FragmentPagerAdapter(fm){
 
     // 프래그먼트 페이지마다 탭레이아웃의 제목이 뭔가요
     override fun getPageTitle(position: Int): CharSequence? {
-        if(position == 0){
-            return "이름"
-        }else if(position == 1){
-            return "내 정보"
-        }else{
-            return "인사말"
+        return when (position){
+            0 -> "이름"
+            1 -> "내 정보"
+            else -> "인사말"
         }
     }
 
 
     // 몇번의 포지션에따라 어떤 프래그먼트가 나와야하는지 물어보는 함수
     override fun getItem(position: Int): Fragment {
-        if(position == 0){
-            return NameFragment()
-        }else if(position == 1){
-            return MyinfoFragment()
-        }else{
-            return GreetingsFragment()
+        return when (position){
+            0 -> NameFragment()
+            1 -> MyinfoFragment()
+            else -> GreetingsFragment()
         }
     }
 
